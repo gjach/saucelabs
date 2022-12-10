f04948276ffbf6d43e453825d4707c0b34e14bd8
gjach <ivoberg12@gmail.com>
2022-12-10 08:16:07 +0100

    Listing tests

--- src/test/java/ListingTest.java
+++ src/test/java/ListingTest.java
@@ -56,4 +56,54 @@ public class ListingTest extends BaseTest {
         Collections.sort(beforeProductsFromZToAList, Collections.reverseOrder());
         Assert.assertEquals(beforeProductsFromZToAList, afterProductsFromZToAList);
     }
+    @Test
+    public void sortingByPriceLowToHigh() {
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        List<WebElement> beforeSortingLowToHigh = driver.findElements(By.cssSelector(".inventory_item_price"));
+        List<Double> beforeSortingLowToHighList = new ArrayList<>();
+        for(WebElement p : beforeSortingLowToHigh){
+            beforeSortingLowToHighList.add(Double.valueOf(p.getText().replace("$", "")));
+        }
+        WebElement sortButton = driver.findElement(sortButtonLocator);
+        sortButton.click();
+        WebElement sortLowToHigh = driver.findElement(By.cssSelector("option[value='lohi']"));
+        sortLowToHigh.click();
+
+        List<WebElement> afterSortingLowToHigh = driver.findElements(By.cssSelector(".inventory_item_price"));
+        List<Double> afterSortingLowToHighList = new ArrayList<>();
+        for(WebElement p : afterSortingLowToHigh){
+            afterSortingLowToHighList.add(Double.valueOf(p.getText().replace("$", "")));
+        }
+
+        Collections.sort(beforeSortingLowToHighList);
+        Assert.assertEquals(beforeSortingLowToHighList, afterSortingLowToHighList);
+    }
+    @Test
+    public void sortingByPriceHighToLow() {
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        List<WebElement> beforeSortingHighToLow = driver.findElements(By.cssSelector(".inventory_item_price"));
+        List<Double> beforeSortingHighToLowList = new ArrayList<>();
+        for(WebElement p : beforeSortingHighToLow){
+            beforeSortingHighToLowList.add(Double.valueOf(p.getText().replace("$", "")));
+        }
+        WebElement sortButton = driver.findElement(sortButtonLocator);
+        sortButton.click();
+        WebElement sortHighToLow = driver.findElement(By.cssSelector("option[value='hilo']"));
+        sortHighToLow.click();
+
+        List<WebElement> afterSortingHighToLow = driver.findElements(By.cssSelector(".inventory_item_price"));
+        List<Double> afterSortingHighToLowList = new ArrayList<>();
+        for(WebElement p : afterSortingHighToLow){
+            afterSortingHighToLowList.add(Double.valueOf(p.getText().replace("$", "")));
+        }
+
+        Collections.sort(beforeSortingHighToLowList, Collections.reverseOrder());
+        Assert.assertEquals(beforeSortingHighToLowList, afterSortingHighToLowList);
+    }
 }
