a5a7e6b3c8fcbcb86ade4fb125e3a8673d42b44e
gjach <ivoberg12@gmail.com>
2022-12-13 19:50:19 +0100

    Checkout Tests

--- src/test/java/CheckoutTest.java
+++ src/test/java/CheckoutTest.java
@@ -20,5 +20,114 @@ public class CheckoutTest extends BaseTest{
         List<WebElement> errorMessages = driver.findElements(By.cssSelector("[class*='input_error']"));
         Assert.assertEquals(errorMessages.size(), 3);
     }
+    @Test
+    public void instertOnlyName (){
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+        driver.findElement(By.cssSelector(".shopping_cart_link")).click();
+        driver.findElement(By.id("checkout")).click();
+        driver.findElement(By.id("first-name")).sendKeys("John");
+        driver.findElement(By.id("continue")).click();
+        WebElement errorMessageFrame = driver.findElement(By.cssSelector("[class*=error-message-container]"));
+        String expectedErrorMessage = "Error: Last Name is required";
+        Assert.assertEquals(errorMessageFrame.getText(), expectedErrorMessage);
+    }
+    @Test
+    public void instertOnlySurname (){
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+        driver.findElement(By.cssSelector(".shopping_cart_link")).click();
+        driver.findElement(By.id("checkout")).click();
+        driver.findElement(By.id("last-name")).sendKeys("Wick");
+        driver.findElement(By.id("continue")).click();
+        WebElement errorMessageFrame = driver.findElement(By.cssSelector("[class*=error-message-container]"));
+        String expectedErrorMessage = "Error: First Name is required";
+        Assert.assertEquals(errorMessageFrame.getText(), expectedErrorMessage);
+    }
+    @Test
+    public void instertOnlyPostalCode (){
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+        driver.findElement(By.cssSelector(".shopping_cart_link")).click();
+        driver.findElement(By.id("checkout")).click();
+        driver.findElement(By.id("postal-code")).sendKeys("800552");
+        driver.findElement(By.id("continue")).click();
+        WebElement errorMessageFrame = driver.findElement(By.cssSelector("[class*=error-message-container]"));
+        String expectedErrorMessage = "Error: First Name is required";
+        Assert.assertEquals(errorMessageFrame.getText(), expectedErrorMessage);
+    }
+    @Test
+    public void instertAllWithoutPostalCode (){
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+        driver.findElement(By.cssSelector(".shopping_cart_link")).click();
+        driver.findElement(By.id("checkout")).click();
+        driver.findElement(By.id("first-name")).sendKeys("John");
+        driver.findElement(By.id("last-name")).sendKeys("Wick");
+        driver.findElement(By.id("continue")).click();
+        WebElement errorMessageFrame = driver.findElement(By.cssSelector("[class*=error-message-container]"));
+        String expectedErrorMessage = "Error: Postal Code is required";
+        Assert.assertEquals(errorMessageFrame.getText(), expectedErrorMessage);
+    }
+    @Test
+    public void instertAllWithoutSurname (){
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+        driver.findElement(By.cssSelector(".shopping_cart_link")).click();
+        driver.findElement(By.id("checkout")).click();
+        driver.findElement(By.id("first-name")).sendKeys("John");
+        driver.findElement(By.id("postal-code")).sendKeys("800552");
+        driver.findElement(By.id("continue")).click();
+        WebElement errorMessageFrame = driver.findElement(By.cssSelector("[class*=error-message-container]"));
+        String expectedErrorMessage = "Error: Last Name is required";
+        Assert.assertEquals(errorMessageFrame.getText(), expectedErrorMessage);
+    }
+    @Test
+    public void instertAllWithoutName (){
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+        driver.findElement(By.cssSelector(".shopping_cart_link")).click();
+        driver.findElement(By.id("checkout")).click();
+        driver.findElement(By.id("last-name")).sendKeys("Wick");
+        driver.findElement(By.id("postal-code")).sendKeys("800552");
+        driver.findElement(By.id("continue")).click();
+        WebElement errorMessageFrame = driver.findElement(By.cssSelector("[class*=error-message-container]"));
+        String expectedErrorMessage = "Error: First Name is required";
+        Assert.assertEquals(errorMessageFrame.getText(), expectedErrorMessage);
+    }
+    @Test
+    public void checkContinueButton (){
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+        driver.findElement(By.cssSelector(".shopping_cart_link")).click();
+        driver.findElement(By.id("checkout")).click();
+        driver.findElement(By.id("first-name")).sendKeys("John");
+        driver.findElement(By.id("last-name")).sendKeys("Wick");
+        driver.findElement(By.id("postal-code")).sendKeys("800552");
+        driver.findElement(By.id("continue")).click();
+        String expectedUrl = "https://www.saucedemo.com/checkout-step-two.html";
+        Assert.assertEquals(driver.getCurrentUrl(), expectedUrl);
+    }
 }
 
