d7f00eff70f49c2267cfb2b827d7d71761144d67
gjach <ivoberg12@gmail.com>
2023-03-12 12:50:25 +0100

    test

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -72,8 +72,8 @@ public class BaseTest {
     public void getResult(ITestResult result) throws Exception {
         if (result.getStatus() == ITestResult.FAILURE) {
             logger.log(Status.FAIL, result.getThrowable());
-            String screenshotPath = captureScreenshot(driver);
-            logger.fail("Screenshot: ", MediaEntityBuilder.createScreenCaptureFromBase64String(screenshotPath).build());
+            String screenshot = captureScreenshot(driver);
+            logger.fail("Screenshot: ", MediaEntityBuilder.createScreenCaptureFromBase64String(screenshot).build());
         } else if (result.getStatus() == ITestResult.SUCCESS) {
             logger.log(Status.PASS, result.getTestName());
         } else {
@@ -88,6 +88,13 @@ public class BaseTest {
 
         TakesScreenshot scrShot = ((TakesScreenshot) driver);
         String screenshot = scrShot.getScreenshotAs(OutputType.BASE64);
+//        String methodName = result.getMethod().getMethodName();
+//        String timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(new Date());
+//        String fileName = "/screenshots/" + methodName + "_" + timeStamp + ".png";
+//        String destination = System.getProperty("user.dir") + fileName;
+//        File DestFile = new File(destination);
+//        FileHandler.copy(SrcFile, DestFile);
+//        String pathForJenkins = "http://localhost:8080/job/newtest/ws" + fileName;
         return screenshot;
     }
 }
